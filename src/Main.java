9235bb01654c9eb6d1a36f7c0f622a9c2d99f45b
Nidhi Agrawal <nidhiagr1327@gmail.com>
2022-02-06 22:46:53 +0530

    6th feb final commit

--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-
+        while(true) {
+       Inventory invnt= new Inventory();
        OrderPlacement op=new OrderPlacement();
        Scanner sc=new Scanner(System.in);
-
-        boolean entry=true;
-        while(entry==true) {
-            op.mainMenu();
+        op.mainMenu();
             int option=sc.nextInt();
             switch(option){
-                case 1:
-                    //view ingridients after reading from text file
+                case 1:invnt.getInventory();
                 case 2:
-                case 3:
+                case 3:op.totalSales();
                 case 4:
                 case 5:
                 case 6:
-                case 7:
-                    entry=false;
+                case 7:break;
+
 
             }
         }
