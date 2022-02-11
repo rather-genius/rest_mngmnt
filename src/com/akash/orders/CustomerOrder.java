f51b0cffd0739ac14209507d29204d13be1c9dc5
Akashokshelke96 <aryanakashshelke@gmail.com>
2022-02-11 23:20:31 +0530

    hackathon practice

--- src/com/akash/orders/CustomerOrder.java
+++ src/com/akash/orders/CustomerOrder.java
@@ -3,43 +3,50 @@ package com.akash.orders;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import static com.sun.org.apache.xalan.internal.lib.ExsltStrings.split;
 
 public class CustomerOrder {
-    int quantity;
+
+    Scanner scan = new Scanner(System.in);
+    String dish = scan.next();
+    int quantity = scan.nextInt();
 
     public void order(String dish, int quantity) throws FileNotFoundException {
         FileReader recipe = new FileReader("resources//Recipe.txt");
         BufferedReader br = new BufferedReader(recipe);
         List<String> availableDishes = br.lines().collect(Collectors.toList());
-        HashMap<String,Integer> ingredientHashMap = new HashMap();
-
-
+        HashMap<String,Integer> placeOrder = new HashMap();
 
 
         for (String s : availableDishes) {
-            if (getDishName(s).equals(dish)) {
+            if (getDishName(s).equals(dish)){
                 int i =1;
                 String[] parts = s.split(" ");
                 int size = parts.length;
                 while(i<=size-3){
-                    ingredientHashMap.put(getIngredientDetails(s,i),Integer.parseInt(getIngredientDetails(s,i+1)));
+                    placeOrder.put(getIngredientDetails(s,i),Integer.parseInt(getIngredientDetails(s,i+1)));
                     i+=2;
                 }
                 double cost = quantity * getPrice(s);
                 System.out.println(" your order is placed");
 
             }
+            else {
+                System.out.println("Dish Not Found");
+            }
+
+            }
 
 
 
         }
-    }
+
 
     private String getIngredientDetails(String s,int i) {
         String[] parts = s.split(" ");
